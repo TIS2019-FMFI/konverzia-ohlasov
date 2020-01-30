01a7db0ac58f0946ca87d21dd5d15762b6bc2789
IvetBx <IVbalintova@gmail.com>
2020-01-30 21:58:15 +0100

    zmena aby v output subore pri nahrati do virtui detekovalo adresar

--- app1/file_writer/file_writer.py
+++ app1/file_writer/file_writer.py
@@ -47,41 +47,43 @@ class file_writer:
     def __create_leader_of_record(self, data_fields, references):
         # information about record
 
-        length = f'{(self.CONST_LEADER_LENGTH + len(self.CONST_FIELD_SEPARATOR) + len(data_fields)):05}'   #record's length
+        length = f'{(self.CONST_LEADER_LENGTH + len(data_fields)):05}'   #record's length
         status = 'n'
         type = 'a'
         implementation_defined = "b "
         character_coding_scheme = 'a'
         indicator_count = '2'
         subfield_code_length = '2'
-        base_address_of_data = f'{(self.CONST_LEADER_LENGTH + (self.CONST_DIRECTORY_LENGTH * (len(references) + 2))  + len(self.CONST_FIELD_SEPARATOR)):05}'  #start position data field
+        #base_address_of_data = f'{(self.CONST_LEADER_LENGTH + (self.CONST_DIRECTORY_LENGTH * (len(references) + 2))  + len(self.CONST_FIELD_SEPARATOR)):05}'  #start position data field
+        base_address_of_data = f'{(self.CONST_LEADER_LENGTH + (self.CONST_DIRECTORY_LENGTH * (len(references) + 2))):05}'  #start position data field
         implementation_defined2 = "1a "
         entry_map = "4500"
 
         result_record = length + status + type + implementation_defined + character_coding_scheme + indicator_count + subfield_code_length + base_address_of_data + implementation_defined2 + entry_map
 
-        return result_record + self.CONST_FIELD_SEPARATOR
-
+        return result_record
 
     def __create_data_fields_of_record(self, field035, field008, references):
         data_fields = ""
         directory = ""
 
-        data_fields += self.CONST_FIELD_SEPARATOR + field008
-        directory += "008" + f'{len(field008):04}' + "00000"
-        field035_res = self.CONST_FIELD_SEPARATOR + self.CONST_INDICATOR_1 + self.CONST_INDICATOR_2 + \
-                       self.CONST_SUBFIELD_SEPARATOR + 'a' + field035 + self.CONST_FIELD_SEPARATOR
-        data_fields += field035_res
-        directory += "035" + f'{len(field035_res):04}' + f'{len(field008):05}'
+        field008_res = self.CONST_FIELD_SEPARATOR + field008
+        data_fields += field008_res
+        directory += "008" + f'{len(field008_res):04}' + "00000"
 
-        position = len(field008) + len(field035_res)
+        field035_res = self.CONST_FIELD_SEPARATOR + self.CONST_INDICATOR_1 + self.CONST_INDICATOR_2 + \
+                       self.CONST_SUBFIELD_SEPARATOR + 'a' + field035
+        data_fields += field035_res
+        directory += "035" + f'{len(field035_res):04}' + f'{len(field008_res):05}'
+
+        position = len(field008_res) + len(field035_res)
         for i in references:
-            field = self.CONST_INDICATOR_1 + self.CONST_INDICATOR_2 + i.to_iso2709_string() + self.CONST_FIELD_SEPARATOR
+            field = self.CONST_FIELD_SEPARATOR + self.CONST_INDICATOR_1 + self.CONST_INDICATOR_2 + i.to_iso2709_string()
             data_fields += field
             directory += "591" + f'{len(field):04}' + f'{position:05}'
             position += len(field)
 
-        result = directory + data_fields
+        result = directory + data_fields + self.CONST_FIELD_SEPARATOR
         return result
 
     def close(self):
