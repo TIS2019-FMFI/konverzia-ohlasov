a2fae3cd5eadcb8f7a0178261b26c958b5b56299
FilipKerak <f.kerak@gmail.com>
2020-02-07 13:22:45 +0100

    Upraveny leader

--- converter/file_writer.py
+++ converter/file_writer.py
@@ -17,8 +17,8 @@ class file_writer:
             encoding {str} -- kodovanie suboru (default: {"utf-8"})
             path {str} -- cesta kde bude subor ulozeny (default: {""})
         """
-        self.CONST_FIELD_008 = "190625|2018    ne || ||||   ||   ||eng |"
-        self.CONST_FIELD_LEADER="00773nab a22001211a 4500"
+        self.CONST_FIELD_008 = "|2018    ne || ||||   ||   ||eng |"
+        self.CONST_FIELD_LEADER="nab a22001211a 4500"
         self.CONST_INDICATOR_1 = ' '
         self.CONST_INDICATOR_2 = ' '
         self.writer = MARCWriter(open(path + name, 'wb'))
@@ -34,11 +34,14 @@ class file_writer:
 
         if(field008 == ""):
             field008 = self.CONST_FIELD_008
-        record=Record(force_utf8=True, leader=self.CONST_FIELD_LEADER)
+        record=Record(force_utf8=True)
         record.add_field(Field(tag = '008',data=field008))
         record.add_field(Field(tag = '035',indicators = [self.CONST_INDICATOR_1,self.CONST_INDICATOR_2],subfields = ['a', field035]))
         for i in references:
             record.add_field(i.to_marc_field())
+        record.leader=record.leader[:5]+'n'+record.leader[6:]
+        record.leader = record.leader[:7] + 'b' + record.leader[8:]
+        record.leader = record.leader[:18] + 'a' + record.leader[19:]
         self.writer.write(record)
 
     def close(self):
